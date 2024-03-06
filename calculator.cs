87f557e918b08ed482f7e9d26f1415f9bdb3faf5
rianimamul <rianimamul@students.amikom.ac.id>
2024-03-06 17:00:13 +0700

    tekan sembarang key untuk keluar

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,14 @@ class calculator
 {
 	static void main(string[] args)
 	{
+		int a = 10; 
+		int b = 6;
 		
+		console.writeline("hasil penambahan: {0} + {1] = {2}",a, b, penambahan(a, b));
+		console.writeline("hasil pengurangan: {0} - {1] = {2}",a, b, pengurangan(a, b));
+		
+		console.writeline("\nTekan sembarangan key untuk keluar");
+		console.Readkey();
 	}
 	
 	
